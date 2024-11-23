83d785e23dae41f4878961dcd297fde382556163
ckosik1 <christina@na.studio>
2024-11-23 10:36:42 +0800

    Update corsMiddleware.js

--- corsMiddleware.js
+++ corsMiddleware.js
@@ -1,7 +1,7 @@
 // corsMiddleware.js
 
 export default function cors(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Adjust origin as needed https://alterkit.webflow.io
+    res.setHeader('Access-Control-Allow-Origin', 'https://alterkit.webflow.io'); // Adjust origin as needed 
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
